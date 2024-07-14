3622076d9ec72d80be0bc74321dd9d7f6db37fb1
abhik <webdeveloper30112000@gmail.com>
2024-07-14 08:27:43 +0530

    Add Spring Initializer

---
+++ src/main/java/com/dependencyi/dependencyi/DependencyiApplication.java
@@ -0,0 +1,13 @@
+package com.dependencyi.dependencyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyiApplication.class, args);
+	}
+
+}
